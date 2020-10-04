0dac617167ddaa90c37f62da13c65255b792c454
Shinnosuke Yamamoto <yamamoto9302@gmail.com>
2020-10-04 22:51:04 +0900

    fixed the scope for AppComponent

--- app/src/main/java/io/github/mrtry/todolist/di/component/AppComponent.kt
+++ app/src/main/java/io/github/mrtry/todolist/di/component/AppComponent.kt
@@ -4,7 +4,9 @@ import dagger.Component
 import io.github.mrtry.todolist.MainApplication
 import io.github.mrtry.todolist.di.module.ActivityModule
 import io.github.mrtry.todolist.di.module.AppModule
+import javax.inject.Singleton
 
+@Singleton
 @Component(modules = [AppModule::class])
 interface AppComponent {
     fun inject(application: MainApplication)
